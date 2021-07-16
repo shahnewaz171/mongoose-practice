cee9857efb25aa83bc0360ee4e9dbbd22cb5144e
shahnewaz601 <shahnewaz601@gmail.com>
2021-07-16 17:11:12 +0600

    insert all data and update data in mongoDB

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -16,7 +16,7 @@ router.get('/:id', async (req, res) => {
 });
 
 
-//POST A  TODO
+//POST A  TODO [insert single item]
 router.post('/', async (req, res) => {
     const newTodo = new Todo(req.body);
     await newTodo.save((err) => {
@@ -30,19 +30,45 @@ router.post('/', async (req, res) => {
                 message: 'Todo is inserted successfully!'
             })
         }
-    });
+    }); 
 });
 
 
-//POST MULTIPLE TODO
+//POST MULTIPLE TODO [insert multiple items]
 router.post('/all', async (req, res) => {
-
+    await Todo.insertMany(req.body, (err) => {
+        if(err){
+            res.status(500).json({
+                error: 'There is a server site error'
+            })
+        }
+        else{
+            res.status(200).json({
+                message: 'Todo is inserted successfully'
+            })
+        }
+    })
 });
 
 
-//PUT TODO
+//PUT TODO [update single data]
 router.put('/:id', async(req, res) => {
-
+    await Todo.updateOne({_id: req.params.id}, {
+        $set: {
+            status: 'inactive'
+        }
+    }, (err) => {
+        if(err){
+            res.status(500).json({
+                error: 'There is a server site error'
+            })
+        }
+        else{
+            res.status(200).json({
+                message: 'Todo is updated successfully'
+            })
+        }
+    })
 });
 
 
